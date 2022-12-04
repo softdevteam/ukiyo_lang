574c71bf6e7be6088d717104bbfe5a26fd1c84ea
Iti Shree <itishree1999@gmail.com>
2022-12-04 17:11:41 +0000

    Added test to check let x = 1+2;

--- src/lib/vm.rs
+++ src/lib/vm.rs
@@ -16,9 +16,22 @@ pub enum OpCode {
     Minus,
 }
 
-fn compiler(ast: Ast, lexer: &dyn NonStreamingLexer<DefaultLexeme<u32>, u32>) -> Vec<OpCode> {
-    assert!(ast.len() >= 1);
-    dbg!(compiler_expr(&ast[0], lexer))
+fn compiler(
+    ast: Ast,
+    lexer: &dyn NonStreamingLexer<DefaultLexeme<u32>, u32>
+) -> Result<Vec<OpCode>, String> {
+    // assert!(ast.len() >= 1);
+    // dbg!(compiler_expr(&ast[0], lexer))
+    if ast.is_empty() {
+        return Ok(Vec::new());
+    }
+
+    let mut res = Vec::new();
+    for node in ast {
+        let val = compiler_expr(&node, lexer);
+        res.extend(val);
+    }
+    Ok(res)
 }
 
 fn compiler_expr(
@@ -27,24 +40,29 @@ fn compiler_expr(
 ) -> Vec<OpCode> {
     match node {
         config_ast::Expr::Int { span: _, is_negative, val } => {
+            println!("entering Expr::Int");
             let mut tmp = lexer.span_str(*val).parse().unwrap();
+            let mut res = Vec::new();
             if *is_negative {
                 tmp = -1 * tmp;
             }
-            vec![OpCode::Int(tmp)]
+            res.push(OpCode::Int(tmp));
+            res
         }
         config_ast::Expr::Assign { span: _, id, expr } => {
+            println!("entering assignment");
             let mut res = Vec::new();
-            let expr = compiler_expr(expr, lexer);
-            let idx = usize::from_str(lexer.span_str(*id)).unwrap_or(0);
-
             let val = compiler_expr(expr, lexer);
-            res.extend(val);
-            res.push(OpCode::StoreVar(idx));
+            let idx = lexer.span_str(*id);
+
+            res.extend(val.clone());
+            println!("here is val {:?}", val);
+            println!("here is idx {}", idx);
+            //res.push(OpCode::StoreVar(idx));
             res
         }
         config_ast::Expr::BinaryOp { span: _, op, lhs, rhs } => {
-            //todo
+            println!("entering binary op");
             let lhs = compiler_expr(lhs, lexer);
             let rhs = compiler_expr(rhs, lexer);
             let _op = lexer.span_str(*op);
@@ -54,14 +72,16 @@ fn compiler_expr(
                     res.extend(lhs);
                     res.extend(rhs);
                     res.push(OpCode::Plus);
-                    res
+                    println!("res is : {:?}", res);
+                    return res;
                 }
                 "-" => {
                     let mut res = Vec::new();
                     res.extend(lhs);
                     res.extend(rhs);
                     res.push(OpCode::Minus);
-                    res
+                    println!("res is : {:?}", res);
+                    return res;
                 }
                 &_ => todo!(),
             }
@@ -79,7 +99,10 @@ fn compiler_expr(
     }
 }
 
-fn vm(prog: Vec<OpCode>) -> i32 {
+fn vm(prog: Vec<OpCode>) -> Result<i32, String> {
+    if prog.is_empty() {
+        return Err("Cannot execute empty program".to_string());
+    }
     let mut pc = 0;
     let mut stack: Vec<i32> = Vec::new();
     let mut locals: HashMap<usize, i32> = HashMap::new();
@@ -120,14 +143,33 @@ fn vm(prog: Vec<OpCode>) -> i32 {
             }
         }
     }
+    let l = stack.len();
+    println!("here is e: {l}");
     //assert_eq!(stack.len(), 1);
-    stack[0]
+    Ok(stack.pop().unwrap())
 }
 
 pub fn run(ast: Ast, lexer: &dyn NonStreamingLexer<DefaultLexeme<u32>, u32>) -> i32 {
-    //let mut locals = HashMap::new();
     let prog = compiler(ast, lexer);
-    vm(prog)
+
+    match prog {
+        Ok(prog_val) => {
+            let result = vm(prog_val);
+            match result {
+                Ok(result_val) => {
+                    return result_val;
+                }
+                Err(err) => {
+                    println!("error: {}", err);
+                    return 0;
+                }
+            }
+        }
+        Err(err) => {
+            println!("error: {}", err);
+            return 0;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -142,11 +184,15 @@ mod test {
         let res = ukiyo_y::parse(&lexer).0.unwrap().unwrap();
         run(res, &lexer)
     }
+    //#[test]
+    // fn basic() {
+    //     assert_eq!(compile_and_run("2+3;"), 5);
+    //     assert_eq!(compile_and_run("2+3+4;"), 9);
+    //     assert_eq!(compile_and_run("2 + -3;"), -1);
+    //     assert_eq!(compile_and_run("2 - 3"), -1);
+    // }
     #[test]
-    fn basic() {
-        assert_eq!(compile_and_run("2+3;"), 5);
-        assert_eq!(compile_and_run("2+3+4;"), 9);
-        assert_eq!(compile_and_run("2 + -3;"), -1);
-        assert_eq!(compile_and_run("2 - 3"), -1);
+    fn test2() {
+        assert_eq!(compile_and_run("let x = 1+2"), 3);
     }
 }
