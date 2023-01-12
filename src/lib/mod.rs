88766c84593b0751bd1f73d9a66abf8318318d47
iti shree <itishree1999@gmail.com>
2023-01-12 10:40:21 +0000

    Allow newlines in testfiles.

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -10,21 +10,15 @@ lrpar_mod!("lib/ukiyo.y");
 pub fn compile(contents: String) {
     // Use the contents string as needed within the function
     let lexerdef = ukiyo_l::lexerdef();
-    let lines: Vec<&str> = contents.lines().collect();
-    for line in lines {
-        if line.trim().is_empty() {
-            continue;
+    let lexer = lexerdef.lexer(&contents);
+    let (res, errs) = ukiyo_y::parse(&lexer);
+    for e in errs {
+        println!("{}", e.pp(&lexer, &ukiyo_y::token_epp));
+    }
+    match res {
+        Some(Ok(r)) => {
+            println!("{:?}", run(r, &lexer));
         }
-        let lexer = lexerdef.lexer(line);
-        let (res, errs) = ukiyo_y::parse(&lexer);
-        for e in errs {
-            println!("{}", e.pp(&lexer, &ukiyo_y::token_epp));
-        }
-        match res {
-            Some(Ok(r)) => {
-                println!("{:?}", run(r, &lexer));
-            }
-            _ => eprintln!("Unable to evaluate expression."),
-        }
+        _ => eprintln!("Unable to evaluate expression."),
     }
 }
